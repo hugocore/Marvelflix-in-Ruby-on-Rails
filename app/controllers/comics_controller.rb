d66a8eadfb5438ad3ffadf169b18224e0ecd0599
Hugo Seq <core.desenvolvimento@creationslabs.pt>
2016-09-18 18:05:51 +0100

    Use offsets and limits to paginate results

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -1,6 +1,6 @@
 class ComicsController < ApplicationController
   def index
-    context = Comics::Index::Base.perform()
+    context = Comics::Index::Base.perform(offset: params[:offset], limit: params[:limit])
 
     @comics = context.comics
   end
