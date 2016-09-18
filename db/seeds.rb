67481d7a0398f6fe8379ae30c01e58483bc7f693
Hugo Seq <core.desenvolvimento@creationslabs.pt>
2016-09-18 19:57:44 +0100

    Add seeds to create a demo user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#########
+# Users #
+#########
+
+puts 'Creating users...'
+
+user = User.where(
+  email: 'email@domain.com',
+).first_or_initialize
+
+user.update(password: 'helloworld')
