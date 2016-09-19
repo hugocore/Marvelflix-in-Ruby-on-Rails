3126985b1bea708975936bee584b2fec1d8a3886
Hugo Seq <core.desenvolvimento@creationslabs.pt>
2016-09-19 23:35:41 +0100

    Map the user's up votes against the current comics

--- app/use_cases/comics/index/base.rb
+++ app/use_cases/comics/index/base.rb
@@ -1,11 +1,22 @@
 module Comics
   module Index
     class Base < UseCase::Base
-      depends FetchComics
+      depends FetchComics,
+              FetchUserUpvotes
 
       # Maps cached or new comics data into a collection of Comics
       def perform
-        context.comics = context.comics_data.map { |comic| Comic.new(comic) }
+        context.comics = context.comics_data.map do |comic|
+          Comic.new(comic.merge(upvote: map_comic_upvote(comic['id'])))
+        end
+      end
+
+      private
+
+      # From the set of upvotes for the current user, check if any of those is related with the
+      # comic that we are iterating in this class
+      def map_comic_upvote(comic_id)
+        context.upvotes.select { |upvote| upvote.comic_id == comic_id }
       end
     end
   end
